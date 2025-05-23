db3718b308b914252e0227b9a1584f7f05f7cb60
David Villavicencio <111402717+dsvill06@users.noreply.github.com>
2025-05-23 16:25:06 -0400

    Attemp at initial build

--- app/(login)/login.tsx
+++ app/(login)/login.tsx
@@ -92,7 +92,7 @@ export function Login({ mode = 'signin' }: { mode?: 'signin' | 'signup' }) {
           <div>
             <Button
               type="submit"
-              className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+              className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-[#548000] hover:bg-[#538000a0] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#548000]"
               disabled={pending}
             >
               {pending ? (
@@ -128,7 +128,7 @@ export function Login({ mode = 'signin' }: { mode?: 'signin' | 'signup' }) {
               href={`${mode === 'signin' ? '/sign-up' : '/sign-in'}${
                 redirect ? `?redirect=${redirect}` : ''
               }${priceId ? `&priceId=${priceId}` : ''}`}
-              className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-full shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+              className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-full shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#548000]"
             >
               {mode === 'signin'
                 ? 'Create an account'
--- lib/auth/session.ts
+++ lib/auth/session.ts
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs';
 import { compare, hash } from 'bcryptjs';
 import { SignJWT, jwtVerify } from 'jose';
 import { cookies } from 'next/headers';
